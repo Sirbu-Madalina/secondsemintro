b11cedffe956164e139a21ec416fa35f16fda89d
SirbuMadalina <madalinasirbu197@gmail.com>
2024-02-01 13:20:47 +0100

    css
    roots

---
+++ script.js
@@ -0,0 +1,8 @@
+/* function changeColor (event) {
+    document.documentElement.style.setProperty('--primary-color', event.target.value);
+}     */
+
+
+const changeColor = (event) => {
+    document.documentElement.style.setProperty('--primary-color', event.target.value);
+} 
